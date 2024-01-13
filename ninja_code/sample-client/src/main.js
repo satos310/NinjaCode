5583b5055147961cb52539f9d8f72589b94c4696
satos310 <shirata.310.yama@gmail.com>
2024-01-14 08:07:34 +0900

    [add]BootstrapVueの適用(src/main.jsに記述追加)

--- ninja_code/sample-client/src/main.js
+++ ninja_code/sample-client/src/main.js
@@ -1,6 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// bootstrap-vueからBootstrapVue, IconPluginをインポートする
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+// BoorstrapとBoostrapVueのCSSファイルをインポート
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// BoostrapVueをプロジェクト全体で利用できるようにする
+Vue.use(Bootstrap)
+// BootstrapVueアイコンコンポーネントプラグインの使用
+Vue.use(IconPlugin)
+
 Vue.config.productionTip = false
 
 new Vue({
